e8797e340d5987f9093293eec04c183e57ea9ee1
Paritosh Wankhade <59663483+wpn10@users.noreply.github.com>
2021-06-30 13:07:33 +0530

    Update OOPS.cpp

--- OOPS.cpp
+++ OOPS.cpp
@@ -1,3 +1,7 @@
+// Encapsulation, Abstrtaction, Inheritance, Polymorphism 
+//The most common use of Polymorphism is when a parent class reference is used to 
+// refer to a child class object
+
 #include<bits/stdc++.h>
 using namespace std;
 class abstractemp{ //Contract abstract class
@@ -5,10 +9,11 @@ class abstractemp{ //Contract abstract class
 };
 class employee:abstractemp{ // employee class signed Contract abstractemp 
 private:                    // SDince it signed the Contract need to provide implementation for afp
-    string name;//Encapsulated Properties    
+   // string name;//Encapsulated Properties    
     string comp;//Encapsulated Properties    
-    int age;    //Encapsulated Properties
-    
+    int age;    //private Not accesible by derived class also
+protected:
+    string name; //Same as private but protected is accesible to derived class as well
 public:
     void setname(string Name){
         name = Name;            // Property Encapsulated here
@@ -44,6 +49,34 @@ public:
         }
         else cout<<"NO\n";
     }
+    virtual void work(){//Checks if implementation is in derived classes.
+        cout<<name<<"is checking email\n";
+    }
+};//public employee lets us acess functions in employee outside developer class like afp();
+class developer:public employee{//developer is a derived class(child) && employee super and parent class
+public:// developer has all employee class Property    
+    string langp;
+    developer(string Name, string Company, int Age, string Langp)
+        :employee( Name, Company, Age){
+            langp = Langp;
+    }
+    void fixbug(){
+        cout<<name<<" age "<<getage()<<" fixed bug using "<<langp<<"\n";
+    }
+    void work(){ //Polymorphism many forms work also in employee 
+        cout<<name<<" is coding in "<<langp<<'\n';
+    }
+};
+class teacher:public developer{
+    public:
+    string sub;
+    teacher(string Name, string Company, int Age, string Langp, string Sub)
+        :developer(Name, Company, Age, Langp){
+            sub = Sub;
+        }
+    void preparelesson(){
+        cout<<name<<" is prepaeing "<<sub<<"\n";
+    }
 };
 int main(){
     employee e1 = employee("Paritosh","IIT",22);
@@ -53,4 +86,16 @@ int main(){
     e1.setage(11);
     e1.intro();
     e1.afp();
+    developer d = developer("Alex","nit",19,"C++");
+    d.fixbug();
+    d.afp();
+    teacher t = teacher("SJDB","SDIJVB",13,"DFSDF","ksdjvn");
+    t.preparelesson();
+    d.afp();
+    d.work();
+    employee *e = &d;//Pointer to base class can hold reference to derived class d deep down is employee
+    employee *e2 = &t;
+    e->work();
+    e2->work();
+    
 }
